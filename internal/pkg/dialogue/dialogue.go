6365c4e05036ce1599987c8777231f75b4dcc37e
michaelmeir <michaelmeir1222001@email.com>
2021-03-04 13:49:21 +0100

    beep

--- internal/pkg/dialogue/dialogue.go
+++ internal/pkg/dialogue/dialogue.go
@@ -69,7 +69,7 @@ func (d *DialogueGenerator) GenerateDialogue(text string, font *truetype.Font, w
 		}
 		pt.X += c.PointToFixed(FONTSIZE/2) + c.PointToFixed(float64(len(word))*(FONTSIZE/3))
 		if pt.X+c.PointToFixed(float64(len(word))*FONTSIZE) >= c.PointToFixed(float64(rgba.Bounds().Inset(15).Max.X)) {
-			pt.X = c.PointToFixed(15)
+			pt.X = c.PointToFixed(20)
 			pt.Y += c.PointToFixed(FONTSIZE / 1.25)
 		}
 	}
