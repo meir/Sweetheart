3c20e7f59ded4ba94d3063a294ec2963ea1cb4b6
michaelmeir <michaelmeir1222001@email.com>
2021-03-04 12:51:51 +0100

    changed to buffer

--- internal/app/commands/sleep.go
+++ internal/app/commands/sleep.go
@@ -1,24 +1,19 @@
 package commands
 
 import (
+	"bytes"
 	"image/png"
-	"io"
 
 	"github.com/meir/Sweetheart/internal/pkg/commandeer"
 )
 
 func sleep(meta commandeer.Meta, command string, arguments []string) bool {
-	println("A")
 	image := meta.DialogueGenerator.GenerateDialogue("Sleep is the most important meal of the day!", meta.DialogueGenerator.NormalFont, 500, 200)
-	println("B")
-	reader, writer := io.Pipe()
-	println("C")
-	go png.Encode(writer, image)
-	println("D")
-	defer writer.Close()
-	defer reader.Close()
-	println("E")
-	meta.Session.ChannelFileSend(meta.Message.ChannelID, "file.png", reader)
-	println("F")
+	var buf bytes.Buffer
+	err := png.Encode(&buf, image)
+	if err != nil {
+		panic(err)
+	}
+	meta.Session.ChannelFileSend(meta.Message.ChannelID, "file.png", bytes.NewReader(buf.Bytes()))
 	return true
 }
