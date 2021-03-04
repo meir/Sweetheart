e0cdddb294eedfd547e868ead44becedb0b1bd97
michaelmeir <michaelmeir1222001@email.com>
2021-03-04 12:42:32 +0100

    added debug printlns

--- internal/app/commands/sleep.go
+++ internal/app/commands/sleep.go
@@ -8,14 +8,20 @@ import (
 )
 
 func sleep(meta commandeer.Meta, command string, arguments []string) bool {
+	println("A")
 	image := meta.DialogueGenerator.GenerateDialogue("Sleep is the most important meal of the day!", meta.DialogueGenerator.NormalFont, 500, 200)
+	println("B")
 	reader, writer := io.Pipe()
+	println("C")
 	err := png.Encode(writer, image)
+	println("D")
 	writer.Close()
 	defer reader.Close()
 	if err != nil {
 		panic(err)
 	}
+	println("E")
 	meta.Session.ChannelFileSend(meta.Message.ChannelID, "file.png", reader)
+	println("F")
 	return true
 }
